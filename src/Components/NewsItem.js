dd93c016e5639c21d8c2d89cbd1321797902a5d5
Purav Kapoor <73545057+Alpha-Chax@users.noreply.github.com>
2022-10-03 04:20:48 +0530

    Update NewsItem.js

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,25 +1,22 @@
-import React, { Component } from "react";
+import React from 'react'
 
-export class NewsItem extends Component {
-  render() {
-    let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
-    return (
-      <div className="my-3">
-        <div className="card">
-          <img src={imageUrl? imageUrl : "https://static3.bigstockphoto.com/5/0/4/large1500/405726158.jpg"} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <h3 className="badge bg-light">{source}</h3>
-            <p className="card-text"><small className="text-muted">By {author?author:"Unknown"} on {new Date(date).toGMTString()}</small></p>
-            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-dark btn-sm">
-              Read More 
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  }
+const NewsItem = (props)=> {
+        let { title, description, imageUrl, newsUrl, author, date, source } = props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <img src={!imageUrl ? "https://cdn.dnaindia.com/sites/default/files/styles/half/public/2022/10/02/2543199-study-creates-map-of-milky-way-galaxy-s-ancient-dead-stars.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <span className="badge text-bg-dark">{source}</span>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+     
 }
 
-export default NewsItem;
+export default NewsItem
